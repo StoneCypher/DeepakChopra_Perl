5b75dba98aabaa31de31d3dafc90b6a04833c4e3
John Haugeland <stonecypher@gmail.com>
2014-01-06 11:39:59 -0800

    Remove the other impl notes

--- DeepakChopra.pl
+++ DeepakChopra.pl
@@ -1,12 +1,3 @@
-#  $starts     = array("Experiential truth ", "The physical world ", "Non-judgment ", "Quantum physics ");
-#  $middles    = array("nurtures ", "projects onto ", "imparts reality to ", "constructs with ");
-#  $qualifiers = array("abundance of ", "the barrier of ", "self-righteous ", "potential ");
-#  $finishes   = array("marvel.", "choices.", "creativity.", "actions.");
-
-#  function random_from($uarray) { return $uarray[rand(0, count($uarray)-1)]; }
-
-#  echo implode(" ", array(random_from($starts), random_from($middles), random_from($qualifiers), random_from($finishes) ));
-
 srand;
 
 my @starts     = ("Experiential truth ", "The physical world ", "Non-judgment ",       "Quantum physics ");
